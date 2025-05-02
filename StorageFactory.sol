eecd83c74a9d673daaba84f0da02ebdcd948f71c
Zecozx <zecolie78@gmail.com>
2025-05-02 21:30:22 +0700

    Add files via upload

---
+++ StorageFactory.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18; // version
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract StorageFactory{
+
+    // uint256 public favoriteNumber
+    //  type visibility   name
+
+    SimpleStorage[] public listOfSimpleStorageContracts;
+
+    // f           name                 ()  visibility {}
+    function createSimpleStorageContract() public {
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
+}
